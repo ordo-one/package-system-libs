57b24f01809bdeaf1dd012314e84757e107aceaf
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2024-05-22 15:00:01 +0300

    Initial commit with LZ4 library.

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "package-system-libs",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+    ],
+    products: [
+        .library(
+            name: "lz4",
+            targets: ["lz4"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SystemLibs",
+            dependencies: ["lz4"]
+        ),
+        .systemLibrary(
+            name: "lz4",
+            pkgConfig: "liblz4",
+            providers: [.apt(["liblz4-dev"]), .brew(["lz4"])]
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# package-system-libs
+
+Some system libraries to be impoerted to other projects to avoid conflicts.
